1bff097c5aea05e9b9f91a171da5ac541a8074cf
Marek Valko <xvalko@nymfe67.fi.muni.cz>
2018-09-25 19:06:33 +0200

    Implemented sellcar method

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -29,8 +30,23 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 	}
 
 	@Override
-	public void addCarToStorage(Car car) {
+	public void addCarToStorage(Car car) throws IllegalArgumentException {
+		if (car.getPrice() <0) {
+			throw new IllegalArgumentException("year is not valid");
+		}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
+	}
+
+	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+		List<Car> carsOfSameType = allCars.get(car.getType());
+		if (carsOfSameType.contains(car)) {
+			carsOfSameType.remove(car);
+		}
+		else {
+			throw new RequestedCarNotFoundException("Car not found");
+		}
 	}
 
 }
